f6bb3da7bce66323082eaa7b86b3429b784c4825
bonny1992 <lh_firepower@hotmail.com>
2017-11-03 19:27:38 +0100

    ....

--- vhosts_ops.py
+++ vhosts_ops.py
@@ -9,6 +9,7 @@ def list_vhost_files():
     files = glob.glob(Config['vhosts_files'])
     vhosts = []
     for vhost in files:
+        print(vhost)
         vhost_specs = {}
         with open(vhost, 'r') as opened:
             lines = opened.readlines()
@@ -103,3 +104,6 @@ def load():
         return True
     except:
         return False
+
+
+load()
