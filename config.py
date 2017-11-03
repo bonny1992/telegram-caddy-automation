66cb9f9a347a4dabaa52b4d25497cfe4189c9477
bonny1992 <lh_firepower@hotmail.com>
2017-11-03 19:28:19 +0100

    fixed config

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 Config = {}
 Config['vhosts_path'] = 'C:/servers/caddy/vhosts'
-Config['vhosts_files'] = 'C:/servers/caddy/vhosts*'
+Config['vhosts_files'] = 'C:/servers/caddy/vhosts/*'
 Config['Admins'] = [50125289]
 Config['cert_file'] = 'C:/Users/Bonny1992/.caddy/acme/acme-v01.api.letsencrypt.org/sites/caddybot.bonny.pwcaddybot.bonny.pw.crt'
 Config['key_file'] = 'C:/Users/Bonny1992/.caddy/acme/acme-v01.api.letsencrypt.org/sites/caddybot.bonny.pwcaddybot.bonny.pw.key'
