574501ca539e2049364ecc4afadcdfea11024c4b
bonny1992 <lh_firepower@hotmail.com>
2017-11-07 21:40:43 +0100

    Should have added list command

--- app.py
+++ app.py
@@ -146,6 +146,13 @@ class CaddyBot:
             reply_markup=reply_markup)
         return ConversationHandler.END
 
+    @restricted
+    def list(self, bot, update):
+        self.logger.info('Vhost list view started by {id}'.format(id=update.effective_user.id))
+        vhosts = vhost_ops.list_vhosts_db()
+        message = 'Ecco i vhost al momento attivi:\n{}'.format('\n'.join(vhosts))[:-1]
+        bot.send_message(chat_id=update.message.chat_id, text=message, parse_mode=ParseMode.MARKDOWN)
+
 
 if __name__ == '__main__':
     bot = CaddyBot()
