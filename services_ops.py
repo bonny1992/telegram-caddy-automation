209221dda0127fbaabf8b2c4f2ac281cbb0c287d
bonny1992 <lh_firepower@hotmail.com>
2017-11-08 20:57:59 +0100

    appunto

--- services_ops.py
+++ services_ops.py
@@ -1,4 +1,3 @@
-import wmi
 import win32serviceutil
 
 def stop_service(servicename = 'Caddy'):
@@ -24,12 +23,7 @@ def start_service(servicename = 'Caddy'):
 
 def get_service(servicename = 'Caddy'):
     try:
-        service_handler = wmi.WMI()
-        service = service_handler.Win32_Service(name=servicename)
-        if srv != []:
-            return service.Status
-        return False
-        # status = win32serviceutil.QueryServiceStatus(servicename)
-        # return status
+        status = win32serviceutil.QueryServiceStatus(servicename)
+        return status
     except:
         return False
